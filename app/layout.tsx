9518c7529b77f2317ede08b660fca5c4eb508915
Nikhil Nagar <nikhilnagarcps@gmail.com>
2023-11-12 23:27:35 +0530

    added sheet component with sidebar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Genius AI",
+  title: "AI Genie",
   description: "An AI based SaaS Platform",
 };
 
