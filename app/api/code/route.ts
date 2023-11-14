1e14ab82063be323f405b3389fd20668bc8b24a1
Nikhil Nagar <nikhilnagarcps@gmail.com>
2023-11-14 22:33:16 +0530

    code creator ai completed.

---
+++ app/api/code/route.ts
@@ -0,0 +1,45 @@
+import OpenAI from 'openai';
+import {NextResponse} from "next/server";
+import {auth} from "@clerk/nextjs";
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY 
+});
+
+const instructionMessage = {
+    role:"system",
+    content:"you are a code generator. You must answer only in markdown code snippets. Use code comments for explanations."
+} 
+export async function POST(req:Request) {
+    try{
+        const {userId} = auth();
+        const body = await req.json();
+        const {messages} = body;
+
+        if(!userId){
+            return new NextResponse("Unauthorized",{status:401});
+        }
+
+        if(!openai.apiKey){
+            return new NextResponse("Open AI API key not found.",{status:500})
+        }
+
+        if(!messages){
+            return new NextResponse("Messages are required",{status:400})
+        }
+
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages:[instructionMessage,...messages]
+          });
+
+        return NextResponse.json(response.choices[0].message);
+
+    }
+    catch(error){
+        console.log("[CONVERSATION ERROR]",error);
+        return new NextResponse("Internal Error",{status:500});
+    }
+}
+
+
